67bcaafcbf840379cc88c06163769fdb2ebec9cf
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-02-08 11:40:57 +0100

    remove comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,16 @@
 import api from '@/api'
 import Filters from '@/components/Filters'
 import HomePage from '@/components/HomePage'
-// import { Suspense } from 'react'
 
 export default async function Home() {
   const meals = await api.getByArea('American')
 
   return (
     <>
-      {/* <Suspense> */}
       <Filters
         area={''}
         shownFilters={{ select: true, letters: true }}
       />
-      {/* </Suspense> */}
       <HomePage meals={meals} />
     </>
   )
